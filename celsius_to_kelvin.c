5f1ae482e03dfef2995d26ac7d6e3128bb04c86f
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2022-02-26 13:19:38 +0600

    Create celsius_to_kelvin.c

---
+++ celsius_to_kelvin.c
@@ -0,0 +1,13 @@
+// celsius to Kelvin
+
+#include <stdio.h>
+int main()
+{
+
+    float celsius, kelvin;
+     printf("enter celsius : ");
+     scanf("%f", &celsius);
+     kelvin = 273.15 + celsius;
+     printf("%0.2f Kelvin",kelvin );
+  
+}
